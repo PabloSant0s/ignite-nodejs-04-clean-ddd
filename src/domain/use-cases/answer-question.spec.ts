630d9126a06eee2ac37699ebebe62d68f671b678
PabloSant0s <pablorasantos.dev@gmail.com>
2023-04-25 16:21:13 -0300

    [feat]Dependencias Externas

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,17 @@
 import {expect, test} from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
+import { AnswerRepository } from '../repositories/answers-repository'
 
-test('create an answer', ()=>{
-  const answerQuestion = new AnswerQuestionUseCase()
+const fakeAnswerRepository: AnswerRepository = {
+  async create(answer) {
+    return
+},
+}
 
-  const answer = answerQuestion.execute({
+test('create an answer', async ()=>{
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
+
+  const answer = await answerQuestion.execute({
     instructorId: '1',
     questionId: '1',
     content: 'Nova resposta'
