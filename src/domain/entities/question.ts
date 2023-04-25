b878a621ec456aaff94f78131008e5f85638093d
PabloSant0s <pablorasantos.dev@gmail.com>
2023-04-25 17:02:25 -0300

    [feat]Value Object de slug

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,19 +1,23 @@
 import { randomUUID } from "crypto"
+import { Slug } from "./value-objects/slug"
 
 interface QuestioProps{
   title: string
+  slug: Slug
   content: string
   authorId: string
 }
 export class Question{
   public id: string
   public title: string
+  public slug: Slug
   public content: string
   public authorId: string
 
   constructor(props: QuestioProps, id?: string){
     this.title = props.title
     this.content = props.content
+    this.slug = props.slug
     this.authorId = props.authorId
     this.id = id ?? randomUUID()
   }
