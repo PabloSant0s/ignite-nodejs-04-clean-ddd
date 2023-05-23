27877f658e9f7a7e4af2440ae4ecc3dcda200f4d
PabloSant0s <pablorasantos.dev@gmail.com>
2023-05-23 11:14:16 -0300

    [feat]Caso de uso: Listar comentários da resposta

---
+++ src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
@@ -0,0 +1,44 @@
+import { InMemoryAnswerCommentsRepository } from '@test/repositories/in-memory-answer-comment-repository'
+import { FetchAnswerCommentsUseCase } from './fetch-answer-comments'
+import { makeAnswerComment } from '@test/factories/make-answer-comment'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let answerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: FetchAnswerCommentsUseCase
+
+describe('Fetch Answer Comments Use Case', () => {
+  beforeEach(() => {
+    answerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    sut = new FetchAnswerCommentsUseCase(answerCommentsRepository)
+  })
+
+  it('should be able to fetch answer comments', async () => {
+    for (let index = 0; index < 3; index++) {
+      await answerCommentsRepository.create(
+        makeAnswerComment({ answerId: new UniqueEntityID('answer-1') }),
+      )
+    }
+
+    const { answerComments } = await sut.execute({
+      answerId: 'answer-1',
+      page: 1,
+    })
+
+    expect(answerComments).toHaveLength(3)
+  })
+
+  it('should be able to fetch paginated answer comments', async () => {
+    for (let index = 0; index < 22; index++) {
+      await answerCommentsRepository.create(
+        makeAnswerComment({ answerId: new UniqueEntityID('answer-1') }),
+      )
+    }
+
+    const { answerComments } = await sut.execute({
+      answerId: 'answer-1',
+      page: 2,
+    })
+
+    expect(answerComments).toHaveLength(2)
+  })
+})
--- test/repositories/in-memory-answer-comment-repository.ts
+++ test/repositories/in-memory-answer-comment-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerCommentRepository } from '@/domain/forum/application/repositories/answer-comment-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 
@@ -10,6 +11,12 @@ export class InMemoryAnswerCommentsRepository
     return (
       this.items.find((item) => item.id.toString() === answerCommentId) ?? null
     )
+  }
+
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    return this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
   }
 
   async create(answerComment: AnswerComment) {
