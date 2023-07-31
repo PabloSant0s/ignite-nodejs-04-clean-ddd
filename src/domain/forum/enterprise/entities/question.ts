290b022dc0cda196b0baf5a7b3515eaef9da45a1
Pablo Santos <pablorasantos.dev@gmail.com>
2023-07-31 19:15:51 -0300

    [feat]Classe base de AggregateRoot

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,7 +1,7 @@
-import dayjs from 'dayjs'
-import { Entity } from '@/core/entities/entity'
-import { Optional } from '@/core/types/optional'
+import { AggregateRoot } from '@/core/entities/aggregate-root'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+import dayjs from 'dayjs'
 import { Slug } from './value-objects/slug'
 
 export interface QuestionProps {
@@ -13,7 +13,7 @@ export interface QuestionProps {
   createdAt: Date
   updatedAt?: Date
 }
-export class Question extends Entity<QuestionProps> {
+export class Question extends AggregateRoot<QuestionProps> {
   static create(
     props: Optional<QuestionProps, 'createdAt' | 'slug'>,
     id?: UniqueEntityID,
